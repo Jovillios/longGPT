b4c1741e967c5780dd8c95283f39c778971d35f0
Jovillios <decaestecker.jules@gmail.com>
2025-03-25 16:19:37 +0100

    implement longnet

--- model.py
+++ model.py
@@ -75,6 +75,88 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
+
+class DilatedCausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        self.dilation = config.dilation
+        self.segment_size = config.segment_size
+
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(
+            config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.flash = hasattr(torch.nn.functional,
+                             'scaled_dot_product_attention')
+        if not self.flash:
+            print(
+                "WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                 .view(1, 1, config.block_size, config.block_size))
+
+    def get_mask(self, i, j):
+        """i = row, j=column"""
+        return torch.ones((i, j), device=self.bias.device, dtype=torch.bool).triu(j - i + 2)
+
+    def forward(self, x):
+        B, T, C = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
+        # Dilated attention mechanism
+        padding_len = -T % self.segment_size
+        # pad the sequence to make it a multiple of segment size
+        x = F.pad(x, (0, 0, 0, padding_len))
+        T = T + padding_len
+
+        # Split and sparsify
+        x = x.view(B, -1, self.segment_size, C) # (B, T, C) -> (B, T//segment_size, segment_size, C)    
+        x = x[:, :, ::self.dilation, :] # (B, T//segment_size, segment_size, C) -> (B, T//segment_size, segment_size//dilation, C)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=3) # (B, T//segment_size, segment_size//dilation, 3 * C // n_head)
+        k = k.contiguous().view(B, -1, self.n_head, self.segment_size //
+                            self.dilation, C // self.n_head).transpose(1, 2) # (B, nh, T//segment_size, segment_size//dilation, C // nh)
+        q = q.contiguous().view(B, -1, self.n_head, self.segment_size //
+                                self.dilation, C // self.n_head).transpose(1, 2)
+        v = v.contiguous().view(B, -1, self.n_head, self.segment_size //
+                                self.dilation, C // self.n_head).transpose(1, 2)
+
+        # Causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if self.flash:
+            # efficient attention using Flash Attention CUDA kernels
+            y = torch.nn.functional.scaled_dot_product_attention(
+                q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # (B, nh, T//segment_size, segment_size//dilation, C // nh) x (B, nh, T//segment_size, C // nh, segment_size//dilation) -> (B, nh, T//segment_size, segment_size//dilation, segment_size//dilation)
+            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v  # (B, nh, T//segment_size, segment_size//dilation, segment_size//dilation) x (B, nh, T//segment_size, segment_size//dilation, C // nh) -> (B, nh, T//segment_size, segment_size//dilation, C // nh)
+
+        # scatter the heads back to the sequence length and the head dimension back to the feature dimension
+        zeros = torch.zeros((B, self.n_head, T//self.segment_size, self.segment_size, C // self.n_head), device=x.device)
+        zeros[:, :, :, ::self.dilation, :] = y
+        y = zeros
+
+        # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+
+        return y
+
 class MLP(nn.Module):
 
     def __init__(self, config):
@@ -114,6 +196,8 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    dilation: int = 4 # dilation factor for the dilated causal self-attention
+    segment_size: int = 64 # segment size for the dilated causal self-attention
 
 class GPT(nn.Module):
 
